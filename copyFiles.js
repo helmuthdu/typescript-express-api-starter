3b13896760be82a803bb8bf37c0ffef7d1433ed8
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-06-12 17:31:12 -0300

    fix: missing config files after build;

--- copyFiles.js
+++ copyFiles.js
@@ -4,4 +4,5 @@ const isProd = env === 'production';
 var shell = require('shelljs');
 
 shell.cp('package.json', 'dist/package.json');
+shell.cp('-R', 'config/', 'dist/');
 shell.cp(isProd ? 'pm2/processes.prod.json' : 'pm2/processes.dev.json', 'dist/processes.json');
